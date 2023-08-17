613c4d6c20f368232e61f508d960331244f76c4d
Uros Pesic <129070018+v4lheru@users.noreply.github.com>
2023-08-17 14:50:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,42 @@
+import streamlit as st
+import requests
+import json
+
+# Title of the application
+st.title('Keyword-based Blog Generator')
+
+# Text input for the user to enter keywords
+keywords = st.text_input('Enter the keywords for the blog:')
+
+# Button to generate the blog
+if st.button('Generate Blog'):
+    # Constructing the prompt
+    prompt = f"Write a blog post about {keywords}"
+
+    # API Endpoint
+    url = "https://api.anthropic.com/v1/complete"
+
+    # Headers
+    headers = {
+        'accept': 'application/json',
+        'anthropic-version': '2023-06-01',
+        'content-type': 'application/json',
+        'x-api-key': '$ANTHROPIC_API_KEY' # Replace with your actual API key
+    }
+
+    # Data
+    data = {
+        "prompt": prompt,
+        "max_tokens": 1000 # You can adjust this based on the desired length of the blog
+    }
+
+    # Sending POST request to Claude API
+    response = requests.post(url, headers=headers, json=data)
+
+    # Handling the response
+    if response.status_code == 200:
+        blog_content = json.loads(response.text).get('choices', [{}])[0].get('text', '')
+        st.write(blog_content) # Displaying the generated blog content
+    else:
+        st.error(f"An error occurred: {response.text}")
 
