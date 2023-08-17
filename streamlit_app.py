d191569dc1dce2a2e0485694a4c7b0be663a248f
Uros Pesic <129070018+v4lheru@users.noreply.github.com>
2023-08-17 15:56:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ YOUR_GENERATED_SECRET = "your_secret_here"
 url = "https://api.promptperfect.jina.ai/q2jb2ZpjQ23hUPjN4Xza"
 
 headers = {
-    "x-api-key": f"token {YOUR_GENERATED_SECRET}",
+    "x-api-key": f"token {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}",
     "Content-Type": "application/json"
 }
 
