eade7d4d094055d683e4b4cc402f90df759ade01
Uros Pesic <129070018+v4lheru@users.noreply.github.com>
2023-08-17 15:51:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,25 @@
 import streamlit as st
-from anthropic import Claude2
-from langchain import PromptTemplate
+import requests
+
+API_URL = "https://flowise-production-647d.up.railway.app/api/v1/prediction/52211d51-9c75-4e75-9331-1f5d76a3a5fa"
+
+def query(payload):
+    response = requests.post(API_URL, json=payload)
+    return response.json()
+
+def generate_response(topic):
+    # Prompt
+    template = 'As an experienced data scientist and technical writer, generate an outline for a blog about {topic}.'
+    prompt_query = template.format(topic=topic)
+    # Query Claude-2 model using Flowise API and print out response
+    response = query({"question": prompt_query})
+    return st.info(response['answer'])  # Assuming 'answer' key in the response
 
 st.set_page_config(page_title="🦜🔗 Blog Outline Generator App")
 st.title('🦜🔗 Blog Outline Generator App')
-anthropic_api_key = st.sidebar.text_input('Anthropic API Key', type='password')
-
-def generate_response(topic):
-  claude2_model = Claude2(model_name='claude-2', anthropic_api_key=anthropic_api_key)
-  # Prompt
-  template = 'As an experienced data scientist and technical writer, generate an outline for a blog about {topic}.'
-  prompt = PromptTemplate(input_variables=['topic'], template=template)
-  prompt_query = prompt.format(topic=topic)
-  # Run Claude-2 model and print out response
-  response = claude2_model(prompt_query)
-  return st.info(response)
 
 with st.form('myform'):
-  topic_text = st.text_input('Enter keyword:', '')
-  submitted = st.form_submit_button('Submit')
-  if not anthropic_api_key.startswith('sk-'):
-    st.warning('Please enter your Anthropic API key!', icon='⚠')
-  if submitted and anthropic_api_key.startswith('sk-'):
-    generate_response(topic_text)
+    topic_text = st.text_input('Enter keyword:', '')
+    submitted = st.form_submit_button('Submit')
+    if submitted:
+        generate_response(topic_text)
