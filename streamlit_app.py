8acb391ce6f2ab68bae3dbf8c088d23f0b1017b4
Uros Pesic <129070018+v4lheru@users.noreply.github.com>
2023-08-17 14:57:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,28 @@
 import streamlit as st
-import requests
-import json
+from chatbots.claude import Claude
 
-# Title of the application
-st.title('Keyword-based Blog Generator')
+# Configuration
+ANTHROPIC_API_KEY = "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"  # Replace with your actual API key
+model = "claude-2"  # You can replace this with your desired model
+temperature = 0.1
 
-# Text input for the user to enter keywords
+st.title("Keyword-based Blog Generator")
+
+# Initialization
+if "bot" not in st.session_state:
+    st.session_state.bot = Claude(api_key=ANTHROPIC_API_KEY, model=model, temperature=temperature)
+
+# Input for keywords
 keywords = st.text_input('Enter the keywords for the blog:')
 
-# Button to generate the blog
+# Generate blog button
 if st.button('Generate Blog'):
     # Constructing the prompt
     prompt = f"Write a blog post about {keywords}"
 
-    # API Endpoint
-    url = "https://api.anthropic.com/v1/complete"
+    # Generating the blog content
+    full_response = ""
+    for response in st.session_state.bot.ask_llm(prompt, stream=True):
+        full_response += response
 
-    # Headers
-    headers = {
-        'accept': 'application/json',
-        'anthropic-version': '2023-06-01',
-        'content-type': 'application/json',
-        'x-api-key': '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' # Replace with your actual API key
-    }
-
-    # Data
-    data = {
-        "prompt": prompt,
-        "max_tokens": 1000 # You can adjust this based on the desired length of the blog
-    }
-
-    # Sending POST request to Claude API
-    response = requests.post(url, headers=headers, json=data)
-
-    # Handling the response
-    if response.status_code == 200:
-        blog_content = json.loads(response.text).get('choices', [{}])[0].get('text', '')
-        st.write(blog_content) # Displaying the generated blog content
-    else:
-        st.error(f"An error occurred: {response.text}")
-
+    st.write(full_response)  # Displaying the generated blog content
